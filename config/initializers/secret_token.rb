730eb5409f431d7401248daaa0429b0d3d5a71e6
Tim Ludi <tludi00@gmail.com>
2013-02-19 16:28:45 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tract::Application.config.secret_token = '7208e3a3af7c584361fdce248bd06e7630ed9708199fd33728b3f56bb5ebda82de8f67d136e585134b11223bf110c3e45f2f827ce182aa3424b6498d6289c940'
